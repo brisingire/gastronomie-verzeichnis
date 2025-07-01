b2d8ae792d0c2483306a7de0761206b15e18de71
Jonas Sebastian Amthor <kontakt@jonas-amthor.de>
2025-07-01 22:40:17 +0200

    erneuerung

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,31 @@
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Script from "next/script";
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="de">
+      <head>
+        {/* Google Analytics Tag */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5H7YH2QJVS"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-5H7YH2QJVS');
+            `,
+          }}
+        />
+      </head>
       <body className="flex min-h-screen flex-col bg-gray-50">
         {/* 1) Einmalig der Header oben */}
         <Header />
